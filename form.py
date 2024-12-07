0a973d04c9d9938f117786379aec0f6f487d23cb
cmanh75 <npcm752004t2k29@gmail.com>
2024-12-07 19:39:24 +0700

    update

--- form.py
+++ form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FileField
+from wtforms import StringField, SubmitField, FileField, SelectField
 from wtforms.validators import DataRequired
 from flask_ckeditor import CKEditorField
+from datetime import datetime as dt, timedelta
 
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
@@ -17,5 +18,27 @@ class LoginForm(FlaskForm):
 class UploadForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     price = StringField("Price", validators=[DataRequired()])
+    number = StringField("Number", validators=[DataRequired()])
     image = FileField("Image", validators=[DataRequired()])
     submit = SubmitField("ADD")
+
+class CheckoutForm(FlaskForm):
+    delivery_options = SelectField(
+        "Delivery Date", 
+        choices=[
+            ('1', f"{(dt.now() + timedelta(days=1)).strftime("%B %d, %Y")}"),
+            ('2', f"{(dt.now() + timedelta(days=3)).strftime("%B %d, %Y")}"),
+            ('3', f"{(dt.now() + timedelta(days=5)).strftime("%B %d, %Y")}")
+        ],
+        validate_choice=False
+    )
+    address = StringField("Address", validators=[DataRequired()])
+    submit = SubmitField("OK")
+
+class Confirm(FlaskForm):
+    feedback = CKEditorField("Feedback")
+    accept = SubmitField("Accept")
+    refuse = SubmitField("Refuse")
+
+class PlaceOrder(FlaskForm):
+    submit = SubmitField("Place Your Order")
--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from flask_ckeditor import CKEditor
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String, Boolean
-from form import RegisterForm, LoginForm, UploadForm
+from form import RegisterForm, LoginForm, UploadForm, CheckoutForm, PlaceOrder, Confirm
 from flask_bootstrap import Bootstrap5
+from typing import List, Optional
+from functools import wraps
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 from werkzeug.utils import secure_filename
+from datetime import datetime as dt, timedelta
+from dotenv import load_dotenv
 import os
-
+load_dotenv()
 
 class Base(DeclarativeBase):
     pass
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "cmanh75deptrai"
+app.config['SECRET_KEY'] = os.environ["secret_key"]
 Bootstrap5(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(model_class=Base)
+ckeditor = CKEditor(app)
 db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -24,26 +30,135 @@ def load_user(user_id):
     return db.session.get(User, user_id)
 
 class User(UserMixin, db.Model):
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String, unique=True)
     name: Mapped[str]= mapped_column(String, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
+    selections = relationship("Selection", back_populates="user")
+    address: Mapped[Optional[str]] = mapped_column(String)
+    shipping: Mapped[Optional[int]] = mapped_column(Integer)
+    total_price: Mapped[Optional[int]] = mapped_column(Integer)
+    delivery_date: Mapped[Optional[str]] = mapped_column(String)
 
 class Product(db.Model):
+    __tablename__ = "products"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
-    price: Mapped[str] = mapped_column(String, nullable=False)
+    number: Mapped[int] = mapped_column(Integer, nullable=False)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
     img_path: Mapped[str] = mapped_column(String, nullable=False)
+    selections = relationship("Selection", back_populates="product")
+
+class Selection(db.Model):
+    __tablename__ = "selections"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    product_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("products.id"))
+    user_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+    user = relationship("User", back_populates="selections")
+    product = relationship("Product", back_populates="selections")
+    order_id: Mapped[Optional[int]] = mapped_column(db.ForeignKey("orders.id"))
+    order: Mapped[Optional["Order"]] = relationship(back_populates="selections")
+
+class Order(db.Model):
+    __tablename__ = "orders"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(db.ForeignKey("users.id"))
+    user = relationship("User")
+    selections: Mapped[List["Selection"]] = relationship(back_populates="order")
+    order_date: Mapped[str] = mapped_column(String, nullable=False)
+    confirmed: Mapped[Optional[bool]] = mapped_column(Boolean)
+    feedback: Mapped[Optional[str]] = mapped_column(String)
 
 with app.app_context():
     db.create_all()
 
+@app.route('/add_to_cart', methods=["POST", "PATCH", "GET"])
+def add_to_cart():
+    product_id = request.args.get("product_id")
+    product = db.session.execute(db.select(Product).where(Product.id == product_id)).scalar()
+    check_exist = db.session.execute(db.select(Selection).where(Selection.product_id == product_id and Selection.user_id == current_user.id)).scalar()
+    if check_exist:
+        check_exist.quantity += 1
+        db.session.commit()
+    else:
+        new_item = Selection(
+            product_id=product.id,
+            user_id = current_user.id,
+            quantity = 1,
+            user = current_user,
+            product = product
+        )
+        db.session.add(new_item)
+        db.session.commit()
+    return redirect(url_for('checkout'))
+
+@app.route('/show_order', methods=["PUT", "PATCH", "GET", "POST"])
+def show_order():
+    form = Confirm()
+    order_id = request.args.get("order_id")
+    order = db.session.execute(db.select(Order).where(Order.id == order_id)).scalar()
+    if form.validate_on_submit():
+        order.feedback = form.feedback.data
+        order.confirmed = False
+        if form.accept.data:
+            order.confirmed = True
+        db.session.commit()
+    return render_template("show_order.html", order=order, form=form)
 
 @app.route('/')
 def home():
-    list = db.session.execute(db.select(Product)).scalars().all()
-    print(list)
-    return render_template('index.html', list=list)
+    return render_template('index.html')
+
+@app.route('/manage')
+def manage():
+    list = db.session.execute(db.select(Order)).scalars().all()
+    return render_template('manage.html', list=list)
+
+@app.route('/checkout', methods=["POST", "GET", "PATCH", "PUT"])
+def checkout():
+    list = db.session.execute(db.select(Selection).where(Selection.user_id == current_user.id)).scalars().all()
+    order_form = CheckoutForm()
+    place_order = PlaceOrder()
+    user = db.session.execute(db.select(User).where(User.id == current_user.id)).scalar()
+    if order_form.submit.data and order_form.validate():
+        user.address = order_form.address.data
+        print(order_form.address.data)
+        days = 1
+        shipping = 50
+        if order_form.delivery_options.data == '2':
+            days = 3
+            shipping = 30
+        if order_form.delivery_options.data == '3':
+            days = 5
+            shipping = 20
+        user.delivery_date = (dt.now() + timedelta(days=days)).strftime("%B %d, %Y")
+        user.shipping = shipping
+        user.total_price = 0
+        for each in list:
+            user.total_price += each.product.price * each.quantity
+        db.session.commit()
+        return render_template("checkout.html", _list=list, filled=True, user=user, order_form=order_form, place_order=place_order)
+    order_form.address.data = user.address
+    if place_order.submit.data and place_order.validate():
+        order = Order(
+            selections=list,
+            user_id=current_user.id,
+            user=current_user,
+            order_date=dt.now().strftime("%B %d, %Y"),
+            confirmed=False
+        )
+        db.session.add(order)
+        db.session.commit()
+        return render_template('checkout.html', _list=list, filled=False, user=None, order_form=order_form, place_order=place_order)
+    return render_template('checkout.html', _list=list, filled=False, user=None, order_form=order_form, place_order=place_order)
+
+@app.route('/delete-item', methods=["GET", "DELETE"])
+def delete_item():
+    db.session.delete(db.session.execute(db.select(Selection).where(Selection.id == request.args.get("item_id"))).scalar())
+    db.session.commit()
+    return redirect(url_for('checkout'))
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
@@ -73,6 +188,25 @@ def login():
         return render_template('login.html', form=form, valid=True)
     return render_template('login.html', form=form, valid=False)
 
+@app.route('/edit', methods=["GET", "POST", "PUT"])
+def edit():
+    print(1)
+    form = UploadForm()
+    if form.validate_on_submit():
+        print(1)
+        product = db.session.execute(db.select(Product).where(Product.id == request.args.get("product_id"))).scalar()
+        f = form.image.data
+        filename = secure_filename(f.filename)
+        path = os.path.join(app._static_folder, 'assets\\img', filename)
+        product.name = form.name.data
+        product.price = form.price.data
+        product.img_path = path
+        product.number = form.number.data
+        f.save(path)
+        db.session.commit()
+        return redirect(url_for('products'))
+    return render_template('add.html', form=form)
+
 @app.route('/add', methods=["POST", "GET"])
 def add():
     form = UploadForm()
@@ -83,7 +217,8 @@ def add():
         new_product = Product(
             name=form.name.data,
             price=form.price.data,
-            img_path=path
+            img_path=path,
+            number=form.number.data
         )
         db.session.add(new_product)
         db.session.commit()
@@ -91,14 +226,28 @@ def add():
         return redirect(url_for('home'))
     return render_template('add.html', form=form)
 
+@app.route('/find', methods=["GET", "POST"])
+def find():
+    if request.method == "POST":
+        name = request.form["name"]
+        list = db.session.execute(db.select(Product)).scalars().all()
+        new_list = []
+        for each in list:
+            if name in each.name:
+                print(each.name)
+                new_list.append(each)
+        return render_template('products.html', list=new_list)
+    return redirect(url_for('home'))
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/product')
-def product():
-    return render_template('products.html')
+@app.route('/products')
+def products():
+    list = db.session.execute(db.select(Product)).scalars().all()
+    return render_template('products.html', list=list)
 
 if __name__ == '__main__':
     app.run(debug=True)
